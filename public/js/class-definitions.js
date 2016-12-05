ca874744656e65fc982612e9a46fbd405739ddf7
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-04 22:28:21 -1000

    House class passing

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1075,6 +1075,11 @@ class Shoe {
  * @param {number} stories How many stories tall the house is
  */
 
+class House {
+  constructor(stories){
+    this.stories = stories;
+  }
+}
 
 /**
  * Step 57
